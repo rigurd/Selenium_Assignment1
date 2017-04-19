2bd62c9e292943c745510b72e0f3148a78f334b9
Rickard Berglund <rigurd@hotmail.com>
2017-04-19 13:11:42 +0200

    Created a Maven Project

---
+++ src/main/java/Package/Assignment_1_RickardBerglund_DiyadinSeker/App.java
@@ -0,0 +1,13 @@
+package Package.Assignment_1_RickardBerglund_DiyadinSeker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
